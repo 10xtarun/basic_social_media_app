7acae5336de3145abaf3c28ba02ffb71e335609f
10xtarun <10xtarun@gmail.com>
2023-05-17 16:04:30 +0530

    mongoose collections and documents

--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,37 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    return res.status(200)
-    .json({
-        message: "Posts fetched successfully.",
-        data: [],
-        error: null,
-    })
+
+    mongoose.connection.db.collection("posts")
+        .find()
+        .toArray()
+        .then((documents) => {
+            return res.status(200)
+                .json({
+                    message: "Posts fetched successfully.",
+                    data: documents,
+                    error: null,
+                })
+        })
 })
 
 router.post("/", (req, res) => {
     const body = req.body
     console.log(" post body: ", body)
 
-    return res.status(201)
-    .json({
-        message: "Post created successfully.",
-        data: {},
-        error: null,
-    })
+    mongoose.connection.db.collection("posts")
+        .insertOne(body)
+        .then((document) => {
+            return res.status(201)
+                .json({
+                    message: "Post created successfully.",
+                    data: document,
+                    error: null,
+                })
+        })
 })
 
 router.put("/:uid", (req, res) => {
@@ -29,13 +40,13 @@ router.put("/:uid", (req, res) => {
 
     console.log(" put uid: ", uid)
     console.log(" put body: ", body)
-    
+
     return res.status(200)
-    .json({
-        message: "Post updated successfully.",
-        data: {},
-        error: null,
-    })
+        .json({
+            message: "Post updated successfully.",
+            data: {},
+            error: null,
+        })
 })
 
 module.exports = router
